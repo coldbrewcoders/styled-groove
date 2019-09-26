943b8ce326c713a8d4aea5124673e1e4b0169b93
Malcolm Navarro <mnavarro@templuminc.com>
2019-09-25 23:06:03 -0500

    Added a webpack config for webpack to be compatible with the babelfied lib dir

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config }) => {
+  // Set unambiguous for the sourceType to be able to use export in storybook
+  config.module.rules[0].use[0].options.sourceType = "unambiguous";
+
+  return config;
+};
